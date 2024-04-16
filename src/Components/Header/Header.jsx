3aff5828b383165d3cbb78ff9f4a05009d4a4e3c
MishaliaPillay <2435450@students.wits.ac.za>
2024-04-16 14:35:47 +0200

    16/04

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css'; // Import the CSS file
 import ChefHatImage from './ChefHat.png'; // Import the image file
-import "./Header.css"
+
 function Header() {
     return (
-        <>
-            {/* Wrap the content in a Link component */}
-            <Link to="/">
-                
-                {/* Apply the class to the image */}
-                <img className="HeaderPhoto" src={ChefHatImage} alt="chef hat" /><p>Cheese</p>
-            </Link>
-        </>
+        < Header className='Header'>
+           <ul >
+             <li> {/* Wrap the content in a Link component */}
+                 <Link to="/" className="HeaderLink">
+                        {/* Apply the class to the image */}
+                        <img className="HeaderPhoto" src={ChefHatImage} alt="chef hat" />
+                        <span className="HeaderText">Cheese</span>
+                 </Link>
+                </li>
+            </ul>
+        </Header>
     );
 }
 
